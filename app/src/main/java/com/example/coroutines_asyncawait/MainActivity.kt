83bc8b2abcfbebc4662ea937517a07ad8c0ff916
Keshav Pai <keshu.pai@gmail.com>
2025-03-21 08:17:15 +0530

    Initial Commit with coroutines dependency added, coroutines added to calculate the total stock count without using async and await

--- app/src/main/java/com/example/coroutines_asyncawait/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_asyncawait/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
